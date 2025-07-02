e67fada49407121ae1c2ca0ecdf6111006f7f746
idknevo <nevoexists@gmail.com>
2025-07-02 09:38:24 +0300

    exp

--- app/_components/Banner.tsx
+++ app/_components/Banner.tsx
@@ -88,7 +88,7 @@ const Banner = () => {
           </div>
           <div className="slide-up-and-fade">
             <h5 className="text-3xl sm:text-4xl font-anton text-primary mb-1.5">
-              100
+              1k+
             </h5>
             <p className="text-muted-foreground">Hours Worked</p>
           </div>
