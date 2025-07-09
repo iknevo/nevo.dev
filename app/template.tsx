9c772c614b708065c7e24eef67a40b8e50a785e4
idknevo <nevoexists@gmail.com>
2025-07-09 19:59:31 +0300

    template fixes and some stuff

--- app/template.tsx
+++ app/template.tsx
@@ -2,25 +2,10 @@
 
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { usePathname } from "next/navigation";
-import { useEffect } from "react";
 
 gsap.registerPlugin(useGSAP);
 
 export default function Template({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-
-  useEffect(() => {
-    // Reset transition on route change or resize
-    const resetTransition = () => {
-      gsap.set(".page-transition", { yPercent: 100 });
-      gsap.set(".page-transition--inner", { yPercent: 100 });
-    };
-    resetTransition();
-    window.addEventListener("resize", resetTransition);
-    return () => window.removeEventListener("resize", resetTransition);
-  }, [pathname]);
-
   useGSAP(() => {
     const tl = gsap.timeline();
 
@@ -39,8 +24,8 @@ export default function Template({ children }: { children: React.ReactNode }) {
 
   return (
     <div>
-      <div className="page-transition w-screen h-screen fixed top-0 left-0 bg-neutral-900 z-[5]">
-        <div className="page-transition--inner w-screen h-screen fixed top-0 left-0 bg-primary z-[5] translate-y-full"></div>
+      <div className="page-transition w-screen h-screen fixed top-0 left-0 bg-black z-[5]">
+        <div className="page-transition--inner w-screen h-screen fixed top-0 left-0 bg-primary-dark z-[5] translate-y-full"></div>
       </div>
 
       {children}
