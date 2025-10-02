31934589fbe3008f2e97dc4ba7466e7c2b1d860c
Ahmed Abdelhafiez <nevoexists@gmail.com>
2025-10-02 20:56:51 +0300

    fix navbar button position

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
             duration: 1.4,
           }}
         >
-          <main className="container">{children}</main>
+          <main>{children}</main>
         </ReactLenis>
         <Cursor />
         <StickyEmail />
