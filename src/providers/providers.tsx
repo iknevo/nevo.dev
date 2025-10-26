43768c03667aaa16968dca7c642ea1fc05f2208b
Ahmed Abdelhafiez <nevoexists@gmail.com>
2025-10-26 12:44:54 +0300

    suspense

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,12 +1,13 @@
 import { Signature } from "@/src/components/me/signature";
 import { Toaster } from "@/src/components/ui/sonner";
+import { Suspense } from "react";
 import { QueryProvider } from "./query-provider";
 import SheetsProvider from "./sheets-provider";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryProvider>
-      {children}
+      <Suspense>{children}</Suspense>
       <SheetsProvider />
       <Toaster />
       <Signature />
