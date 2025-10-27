910982e4f32d9f7a921b7c73e008de3faeb85ae2
Ahmed Abdelhafiez <nevoexists@gmail.com>
2025-10-27 03:14:29 +0300

    fix

--- src/components/experiences.tsx
+++ src/components/experiences.tsx
@@ -1,6 +1,5 @@
 "use client";
 import SectionTitle from "@/src/components/section-title";
-import { MY_EXPERIENCE } from "@/src/lib/data";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
