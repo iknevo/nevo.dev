17367b08281fb88d3ee019338e8491d517fe34ba
iknevo <nevoexists@gmail.com>
2026-05-28 14:12:42 +0300

    drop NEXT_PUBLIC_APP_URL
    since the client can resolve absolute path and i fixed the server fetch
    issue

--- src/lib/hono.ts
+++ src/lib/hono.ts
@@ -2,5 +2,5 @@ import { hc } from "hono/client";
 
 import type { AppTypes } from "@/src/app/api/[[...routes]]/route";
 
-const client = hc<AppTypes>(process.env.NEXT_PUBLIC_APP_URL ?? "");
+const client = hc<AppTypes>("");
 export const api = client.api;
